a19d27aae388742b0ebd4602ef6bbae20537adb2
AnnaXT <annateng8000@gmail.com>
2023-05-05 00:52:48 -0400

    Fixed collectable spawner

--- Assets/Scripts/collect.cs
+++ Assets/Scripts/collect.cs
@@ -7,14 +7,22 @@ public class collect : MonoBehaviour
     Vector2 floatY;
     float originalY;
     public GameObject explosion;
-    public GameObject collectableSpawner;
+    //public GameObject collectableSpawner;
     public GameObject tileGenerator;
 
     public float floatStrength;
 
+
+    /////
+    public GameObject healthCollectable;
+    public GameObject bombCollectable;
+    private GameObject player;
+    /////
+
     void Start ()
     {
         this.originalY = this.transform.position.y;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update () {
@@ -30,12 +38,42 @@ public class collect : MonoBehaviour
             if (this.CompareTag("health")){
                 print("got health");
                 Destroy(this);
-                collectableSpawner.GetComponent<CollectableSpawner>().decreaseHC(1);
+                //collectableSpawner.GetComponent<CollectableSpawner>().decreaseHC();
+                float x = player.transform.position.x;
+                float y = player.transform.position.y;
+                bool isNeg1 = Random.Range(0,10) < 5;
+                bool isNeg2 = Random.Range(0,10) < 5;
+                float rand1 = Random.Range(0,10);
+                float rand2 = Random.Range(10 - rand1, 10);
+                if (isNeg1)
+                {
+                    rand1 = -rand1;
+                }
+                if (isNeg2)
+                {
+                    rand2 = -rand2;
+                }
+                Vector2 position = new Vector2(x + rand1, y + rand2);
+                GameObject newHC = Instantiate(healthCollectable, position, Quaternion.identity);
             }
             else if (this.CompareTag("Clear")){
                 print("cleared screen");
                 Destroy(this);
-                collectableSpawner.GetComponent<CollectableSpawner>().decreaseBC(1);
+                float x = player.transform.position.x;
+                float y = player.transform.position.y;
+                bool isNeg1 = Random.Range(0,10) < 5;
+                bool isNeg2 = Random.Range(0,10) < 5;
+                float rand1 = Random.Range(0,10);
+                float rand2 = Random.Range(10 - rand1, 10);
+                if (isNeg1){
+                    rand1 = -rand1;
+                }
+                if (isNeg2){
+                    rand2 = -rand2;
+                }
+                Vector2 position = new Vector2(x + rand1, y + rand2);
+                GameObject newBC = Instantiate(bombCollectable, position, Quaternion.identity);
+                //collectableSpawner.GetComponent<CollectableSpawner>().decreaseBC();
             //tileGenerator.GetComponent<TileGenerator>().randChangeZone();
         }
             //Destroy(this);
